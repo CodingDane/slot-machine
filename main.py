c025939ef68fd6a0ce37e82e042f72b7a880f186
alexander voeler <avoeler@gmail.com>
2024-04-09 22:06:22 +0900

    created the get bet method

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
 
 def deposit():
     while True:
@@ -26,9 +28,23 @@ def get_number_of_lines():
             print("Please enter a number.")
     return lines
 
+def get_bet():
+    while True:
+        amount = input("Enter the amount you want to bet on each line: ¥")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between {MIN_BET} - {MAX_BET}.")
+        else:
+            print("Please enter a number.")
+    return amount
+
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    print(balance, lines)
+    bet = get_bet()
+    print(balance, lines, bet)
 
 main()
